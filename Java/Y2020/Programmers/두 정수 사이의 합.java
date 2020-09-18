729baad57b326134b3781621c0906ad6c574ef65
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-09-18 17:18:24 +0900

    두 정수 사이의 합_2009

---
+++ Java/Y2020/Programmers/두 정수 사이의 합.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        if (a == b) {
+            answer = a;
+        } else {
+            int start = Math.min(a, b);
+            int end = Math.max(a, b);
+            
+            for (int i = start; i <= end; i++) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
