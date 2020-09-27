50fa52fe6c4a1fc69b1e96c4283e05537a75734c
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-09-27 21:08:56 +0900

    약수의 합_2009

---
+++ Java/Y2020/Programmers/약수의 합.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (n == 1) {
+            return 1;
+        }
+        
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (i == Math.sqrt(n)) {
+                answer += i;
+            } else {
+                if (n % i == 0) {
+                    answer += (i + n / i);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
