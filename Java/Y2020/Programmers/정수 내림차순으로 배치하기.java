14c93fed5c38f2933021a790ae5db0195a96cdb0
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-09-28 21:18:14 +0900

    정수 내림차순으로 배치하기_2009

---
+++ Java/Y2020/Programmers/정수 내림차순으로 배치하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        String[] arr = String.valueOf(n).split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+        }
+        
+        answer = Long.parseLong(sb.toString());
+        return answer;
+    }
+}
