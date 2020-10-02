cad44ffc419bcda2399a517ed02784be4d7b955a
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-10-02 21:43:40 +0900

    x만큼 간격이 있는 n개의 숫자_2010

---
+++ Java/Y2020/Programmers/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = Long.valueOf(x) * (i + 1);
+        }
+        
+        return answer;
+    }
+}
