5819dc12728e527ca3d1a9280306a191a70631a2
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-10-05 23:06:58 +0900

    해시

---
+++ Java/Y2020/Programmers/위장.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++) {
+            if (map.containsKey(clothes[i][1])) {
+                map.replace(clothes[i][1], map.get(clothes[i][1]) + 1);
+            } else {
+                map.put(clothes[i][1], 1);
+            }
+        }
+        
+        for (int value : map.values()) {
+            answer = answer * (value + 1);
+        }
+        
+        answer = answer - 1;
+        return answer;
+    }
+}
