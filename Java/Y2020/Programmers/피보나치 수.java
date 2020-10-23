302e3f82fb9d91d9e86bb9d4f83e0bfeac91ebb2
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-10-23 21:16:24 +0900

    피보나치 수_2010

---
+++ Java/Y2020/Programmers/피보나치 수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        
+        int n1 = 1, n2 = 1, sum = 0;
+        for (int i = 3; i <= n; i++) {
+            sum = n1 + n2;
+            sum %= 1234567;
+            n1 = n2;
+            n2 = sum;
+        }
+        
+        answer = sum;
+        return answer;
+    }
+}
